b5dd0d7d68f183286060713f31b0b2dbbf8cb970
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-11-30 20:28:45 -0500

    reconfiguring

--- shop/shop.directives.js
+++ shop/shop.directives.js
@@ -8,8 +8,10 @@ angular
       restrict: 'EA',
       templateUrl: 'shop/views/woody.directives.html',
       link: function (scope, element, attributes) {
-
-      },
+           element.on('click', function(event) {
+             element.css('color', 'red');
+           });
+         },
       scope: {
         shop: "="
       },
