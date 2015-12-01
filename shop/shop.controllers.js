d5f64f86856935b4bbd4e121c5dbcaf2caeb6e66
Lauren Farr <laurenfarr@Lauren-MacBook-Pro.local>
2015-12-01 13:01:02 -0500

    added etsy angular ios ionic tab

--- shop/shop.controllers.js
+++ shop/shop.controllers.js
@@ -13,17 +13,20 @@
         if($routeParams.shopId){
           ShopService.getShop($routeParams.shopId).then(function(data){
             var things = data.data.results.filter(function(el) {
-              console.log(el.Shop.shop_id === $routeParams.shopId);
+              // console.log(el.Shop.shop_id === $routeParams.shopId);
               return el.Shop.shop_id === parseInt($routeParams.shopId,10);
             });
             var newThings = things.map(function(el) {
               console.log("YAY",el);
               return {
                 title: el.title,
-                description: el.description
+                description: el.description,
+                price: el.price,
+                
               };
             });
-            vm.shop = newThings;
+            console.log(newThings);
+            vm.shop = newThings ;
           });
          }
 
