99339dc9d0d0d20af690a6414e9db2137428c8be
Lauren Farr <lmfarr123@gmail.com>
2016-08-07 21:45:37 -0400

    Update shop.controllers.js

--- shop/shop.controllers.js
+++ shop/shop.controllers.js
@@ -14,11 +14,9 @@
         if($routeParams.shopId){
           ShopService.getShop($routeParams.shopId).then(function(data){
             var things = data.data.results.filter(function(el) {
-              // console.log(el.Shop.shop_id === $routeParams.shopId);
               return el.Shop.shop_id === parseInt($routeParams.shopId,10);
             });
             var newThings = things.map(function(el) {
-              console.log("YAY",el);
               return {
                 title: el.title,
                 description: el.description,
@@ -26,13 +24,11 @@
                 image: el.image_url_760x100
               };
             });
-            console.log(newThings);
             vm.shop = newThings ;
           });
          }
 
         ShopService.seeDetails($routeParams.shopId).success(function(data){
-                  console.log("custdetail" ,data);
                   vm.shop = data;
                 });
 
