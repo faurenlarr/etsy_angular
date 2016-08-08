6b114d3af599e96ee06e6559c3484e38f4b9d3ef
Lauren Farr <lmfarr123@gmail.com>
2016-08-07 21:47:27 -0400

    Update faves.controllers.js

--- faves/faves.controllers.js
+++ faves/faves.controllers.js
@@ -7,7 +7,6 @@
     .controller('FavesController', function($scope, FavesService, $routeParams) {
       var vm = this;
         FavesService.getFaves().success(function(faves){
-          console.log('thingy: ',faves);
             $scope.faves = faves;
             // if($routeParams.faveId){
             //   $scope.fave = FavesService.getFave($scope.faves,$routeParams.faveId);
@@ -15,7 +14,6 @@
         });
 
         $scope.removeFromFaves = function(shopId){
-            console.log("SHOP ID",shopId);
             FavesService.removeFromFaves(shopId);
           };
 
